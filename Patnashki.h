bdb80d87e4b0d66724ec4c879e5f01a0ea44ca65
VadimBachurin <parkursikinazabroske@gmail.com>
2025-08-11 20:13:16 +0300

    begin

--- Patnashki.h
+++ Patnashki.h
@@ -1,45 +1,9 @@
 #pragma once
-#define VALID_POS(row , col, len)(col < len && col >= 0 && row < len && row >= 0) 
 
-int **form , len;
-int col, row;
-
-
-void initial(); // начальная инициализация
-void step();
+int card[3][3];
+void init();
+void stir();
 void print();
-void mixplayer(); // ручное заполнение поля
-bool iswin(int **a);
-
-
-// ф-ции авто размешивания
-
-bool taskpos();  // проверка на нерешаемость позиции
-void automix(); // авто заполнение поля
-
-
-/* // Все для авто хотьбы
-struct State //хранит текущее состояние
-{
-	int** board; // состояние доски
-	int parrent_index; // это ссылка на предыдущее состояние,
-	// которая позволяет восстановить путь решения после его нахождения
-}; */
-
-int empty_row;
-int empty_col;
-int len_lever; // количество узлов в уровне  
-bool found; // флажок на нахождение кароткого пути 
-int*** lever; 
-int k; // счетчик кол-ва новых узлов
-int uzel; // переменная для того что бы проходиться по старым узлам
-int*** temp_lever; // когда lever занят то информацию о узлах будем записывать сюда 
-void comp_play(); // сдесь нужно проходиться по всему уровню
-void add_uzel(); // добавляет новый узел в temp_lever
-void auto_step(); // шаг (например вверх)
-void uzel_step(); // проход по всему узлу(влево, вправо, вверх)
-
-
 
 
 
