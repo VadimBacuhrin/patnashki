a91e6e1d75bf1c0ce48e0b8fc820bb7637415d09
VadimBachurin <parkursikinazabroske@gmail.com>
2025-08-22 20:36:18 +0300

    add

--- Patnashki.cpp
+++ Patnashki.cpp
@@ -4,7 +4,7 @@
 #include <conio.h>
 using namespace std;
 
-
+BFS alfa;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
@@ -26,8 +26,58 @@ void main()
 		// Запустить авто хотьбу и паттерн BFS
 		break;
 	}
-	cout << "\nИгра закончена";
+	cout << "Игра закончена";
 }
+
+void add(int **cadr)
+{
+	if (len == 0)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				alfa.frames[0][i][j] = cadr[i][j];
+			}
+		}
+		len++;
+	}
+	else
+	{
+		frame* temp = new frame[len];
+		for (int i = 0; i < len; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				for (int k = 0; k < 3; k++)
+				{
+					temp[i][j][k] = alfa.frames[i][j][k];
+				}
+			}
+		}
+		delete[] alfa.frames;
+		alfa.frames = new frame[len+1];
+		for (int i = 0; i < len+1; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				for (int k = 0; k < 3; k++)
+				{
+					if (i == len)
+					{
+						alfa.frames[i][j][k] = cadr[j][k];
+					}
+					else
+					{
+						alfa.frames[i][j][k] = temp[i][j][k];
+					}
+				}
+			}
+		}
+	}
+	len++;
+}
+
 
 void init()
 {
--- Patnashki.h
+++ Patnashki.h
@@ -1,5 +1,5 @@
 #pragma once
-
+int len = -1;
 int card[3][3];
 void init();
 void stir();
@@ -7,4 +7,34 @@ void print();
 void handmove();
 bool is_win();
 
+class frame
+{
+	int cadr[3][3];
+	int id;
+public:
+	frame()
+	{
+		len++;
+		id = len+1;
+	}
+	int* operator[](int i)
+	{
+		return cadr[i];
+	}
+};
 
+class BFS
+{
+	frame *frames;
+public:
+	BFS()
+	{
+		frames = new frame[len+1];
+	}
+	friend void add(int **cadr);
+
+	frame& operator[](int a)
+	{
+		return frames[a];
+	}
+};
